71eec9a259c3f22f5244750de0b69154d4a94cf5
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2024-12-16 19:00:46 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import TaskRouter from "./src/routes/task.routes.js";
 import connectToDataBase from "./src/database/mongoose.database.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 connectToDataBase();
