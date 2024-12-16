2086e59c6aeb016317fc06f7d95b04eeba7fd5d0
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2024-12-15 22:41:18 -0300

    feat: add task controller

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -12,6 +12,65 @@ class TaskController {
       this.res.status(500).json({ error: error.message });
     }
   }
+
+  async getTasksById() {
+    try {
+      const taskId = this.req.params.id;
+      const task = await TaskModel.findById(taskId);
+
+      if (!task) {
+        return this.res.status(404).send("Essa tarefa não foi encontrada");
+      }
+    } catch (error) {
+      this.res.status(500).send(error.message);
+    }
+  }
+
+  async create() {
+    try {
+      const newTask = new TaskModel(req.body);
+      await newTask.save();
+      this.res.status(201).send(newTask);
+    } catch (error) {
+      this.res.status(500).send(error.message);
+    }
+  }
+
+  async update() {
+    try {
+      const taskId = this.req.params.id;
+      const taskData = this.req.body;
+      const taskToUpdate = await TaskModel.findById(taskId);
+
+      const allowedUpdates = ["isCompleted"];
+      const requestUpdates = Object.keys(req.body);
+
+      for (const update of requestUpdates) {
+        if (allowedUpdates.includes(update)) {
+          taskToUpdate[update] = taskData[update];
+        }
+      }
+
+      await taskToUpdate.save();
+      return this.res.status(200).send(taskToUpdate);
+    } catch (error) {
+      return this.res.status(500).send(error.message);
+    }
+  }
+
+  async delete() {
+    const taskId = this.req.params.id;
+    try {
+      const taskToDelete = await TaskModel.findById(taskId);
+      if (!taskToDelete) {
+        return this.res.status(500).send("Essa tarefa não foi encontrada");
+      }
+      const deleteTask = await TaskModel.findByIdAndDelete(taskId);
+      this.res.status(200).send(deleteTask);
+    } catch (error) {
+      this.res.status(500).send(error.message);
+    }
+  }
 }
 
 export default TaskController;
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -9,62 +9,19 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  try {
-    const taskId = req.params.id;
-    const task = await TaskModel.findById(taskId);
-
-    if (!task) {
-      return res.status(404).send("Essa tarefa não foi encontrada");
-    }
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+  return new TaskController(req, res).getTasksById();
 });
 
 router.post("/", async (req, res) => {
-  try {
-    const newTask = new TaskModel(req.body);
-    await newTask.save();
-    res.status(201).send(newTask);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+  return new TaskController(req, res).create();
 });
 
 router.patch("/:id", async (req, res) => {
-  try {
-    const taskId = req.params.id;
-    const taskData = req.body;
-    const taskToUpdate = await TaskModel.findById(taskId);
-
-    const allowedUpdates = ["isCompleted"];
-    const requestUpdates = Object.keys(req.body);
-
-    for (const update of requestUpdates) {
-      if (allowedUpdates.includes(update)) {
-        taskToUpdate[update] = taskData[update];
-      }
-    }
-
-    await taskToUpdate.save();
-    return res.status(200).send(taskToUpdate);
-  } catch (error) {
-    return res.status(500).send(error.message);
-  }
+  return new TaskController(req, res).update();
 });
 
 router.delete("/:id", async (req, res) => {
-  const taskId = req.params.id;
-  try {
-    const taskToDelete = await TaskModel.findById(taskId);
-    if (!taskToDelete) {
-      return res.status(500).send("Essa tarefa não foi encontrada");
-    }
-    const deleteTask = await TaskModel.findByIdAndDelete(taskId);
-    res.status(200).send(deleteTask);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+  return new TaskController(req, res).delete();
 });
 
 export default router;
