f9bd8629bfacf2935278a65f27b87858ea5f5133
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2024-12-17 15:40:43 -0300

    fix: listening to enverioment port

--- index.js
+++ index.js
@@ -14,4 +14,6 @@ connectToDataBase();
 
 app.use("/tasks", TaskRouter);
 
-app.listen(8000, () => console.log("Listening on port 8000"));
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log("Listening on port"));
